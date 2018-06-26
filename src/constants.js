c67eb694d4e37d20667ee156e193ecf69d057630
Andrew Hunt <andrew@hunt.codes>
2018-06-26 11:52:07 -0700

    [wip] add states

--- src/constants.js
+++ src/constants.js
@@ -11,6 +11,26 @@ export const ROUTE_PATHS = {
 };
 
 export const GAME_STAGES = new Enum([
+	// These map to the screens (bolded outlines)
+	//    in the project's readme
+	// Player A Setup states
+	'INITIALIZE_SETUP',
+	'CHOOSE_WAGER',
+	'GAME_ACCEPT_RECEIVED',
+
+	// Player B Setup states
+	'WAITING_FOR_PREFUND_MESSAGE',
+	'CONFIRM_WAGER',
+
+	// Player A Play states
+	
+
+	// general states
+	'WAITING_FOR_PLAYER_B',
+	'WAITING_FOR_CHAIN',
+	'GAME CANCELLED',
+
+	// old states
   'SELECT_CHALLENGER',
   'SELECT_MOVE',
   'WAIT_FOR_OPPONENT_MOVE',
