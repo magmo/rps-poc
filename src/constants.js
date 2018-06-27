2bfb0c47704e5d646de0c15b7fc4f333fc3ceeba
andrew_hunt <andrew.hunt@airbnb.com>
2018-06-27 14:05:55 -0700

    [wip] add more states and a couple more screens

--- src/constants.js
+++ src/constants.js
@@ -11,30 +11,36 @@ export const ROUTE_PATHS = {
 };
 
 export const GAME_STAGES = new Enum([
-	// These map to the screens (bolded outlines)
-	//    in the project's readme
-	// Player A Setup states
-	'INITIALIZE_SETUP',
-	'CHOOSE_WAGER',
-	'GAME_ACCEPT_RECEIVED',
+  // These map to the screens (bolded outlines) in the project's readme flow chart
 
-	// Player B Setup states
-	'WAITING_FOR_PREFUND_MESSAGE',
-	'CONFIRM_WAGER',
-
-	// Player A Play states
-	
-
-	// general states
-	'WAITING_FOR_PLAYER_B',
-	'WAITING_FOR_CHAIN',
-	'GAME CANCELLED',
-
-	// old states
+  // PRE setup state
   'SELECT_CHALLENGER',
+
+  // Player A Setup states
+  'INITIALIZE_SETUP',
+  'CHOOSE_WAGER',
+  'GAME_ACCEPT_RECEIVED',
+
+  // Player B Setup states
+  'CONFIRM_WAGER',
+
+  // Player A Play states
   'SELECT_MOVE',
-  'WAIT_FOR_OPPONENT_MOVE',
-  'REVEAL_WINNER',
+
+  // Player B Play states
+  'SELECT_MOVE_AFTER_OPPONENT',
+
+  // Play states used by both players
+  'REVEAL_WINNER_WITH_PROMPT',
+
+  // Conclude state used by both players
+  'CONLUDE_GAME',
+
+  // general states
+  'WAITING_FOR_PLAYER',
+  'WAITING_FOR_CHAIN',
+  'GAME_CANCELLED_BY_YOU',
+  'GAME_CANCELLED_BY_OPPONENT',
 ]);
 
 export const MOVE_OPTIONS = [
