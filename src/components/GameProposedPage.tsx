4f8946d85b1beba9d5bae91082b96984352209cf
andrew_hunt <andrew.hunt@airbnb.com>
2018-08-27 23:45:58 -0700

    [andrewh] a few improvements

--- src/components/GameProposedPage.tsx
+++ src/components/GameProposedPage.tsx
@@ -7,12 +7,18 @@ interface Props {
 
 export default class ProposeGamePage extends React.PureComponent<Props> {
   render() {
+    const { message } = this.props;
+
     return (
       <div className={css(styles.container)}>
         <div>
           <h1>Game Proposed</h1>
 
           <div>Waiting for your opponent to accept the game!</div>
+
+          <div className={css(styles.footerBar)}>
+            <div className={css(styles.container, styles.message)}>{message}</div>
+          </div>
         </div>
       </div>
     );
@@ -27,5 +33,20 @@ const styles = StyleSheet.create({
 
   fullWidth: {
     width: '100%',
+  },
+
+  message: {
+    paddingTop: 12,
+  },
+
+  footerBar: {
+    position: 'fixed',
+    bottom: 0,
+    height: 50,
+    left: 0,
+    right: 0,
+    borderTopColor: 'black',
+    borderTopStyle: 'solid',
+    borderTopWidth: 1,
   },
 });
