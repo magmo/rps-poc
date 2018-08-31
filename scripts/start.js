ee74fbe13d4673c09574ae6c7d9305e01a3249e9
Alex Gap <alex.gap@gmail.com>
2018-08-31 13:18:34 -0700

    Update start.js
    fixed typo

--- scripts/start.js
+++ scripts/start.js
@@ -8,7 +8,7 @@ process.env.DEV_GANACHE_PORT=7546;
 process.env.DEFAULT_GAS= 6721975;
 process.env.DEFAULT_GAS_PRICE = 20000000000;
 // Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise reejections that are not handled will
+// ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
 process.on('unhandledRejection', err => {
   throw err;
