ac870ea3303df7434ca9afee95a0185c83942bf6
Alex Gap <alex.gap@gmail.com>
2018-09-03 23:11:07 -0700

    Add simple adjudicator deployment

--- src/wallet/redux/actions/blockchain.ts
+++ src/wallet/redux/actions/blockchain.ts
@@ -1,22 +1,33 @@
-import { Wallet } from '../..';
+export type BlockchainDeployAdjudicatorAction = ReturnType<typeof deployAdjudicator>;
+export type BlockchainAdjudicatorDeployedAction = ReturnType<typeof adjudicatorDeployed>;
+export type BlockchainAction =
+  | BlockchainDeployAdjudicatorAction
+  | BlockchainAdjudicatorDeployedAction;
 
-export type BlockchainSendTransactionAction = ReturnType<typeof BlockchainAction.sendTransaction>;
-export type BlockchainReceiveEventAction = ReturnType<typeof BlockchainAction.receiveEvent>;
-export type BlockchainAction = BlockchainSendTransactionAction | BlockchainReceiveEventAction;
+export const BLOCKCHAIN_DEPLOYADJUDICATOR = 'BLOCKCHAIN.DEPLOYADJUDICATOR';
+export const BLOCKCHAIN_ADJUDICATORDEPLOYED = 'BLOCKCHAIN.ADJUDICATORDEPLOYED';
+export const BLOCKCHAIN_METAMASKERROR = 'BLOCKCHAIN.METAMASKERROR';
+export const BLOCKCHAIN_WRONGNETWORK = 'BLOCKCHAIN.WRONGNETWORK';
+export const BLOCKCHAIN_RECEIVEEVENT = 'BLOCKCHAIN.RECEIVEEVENT';
 
-export enum BlockchainActionType {
-  BLOCKCHAIN_SENDTRANSACTION = 'BLOCKCHAIN.SENDTRANSACTION',
-  BLOCKCHAIN_RECEIVEEVENT = 'BLOCKCHAIN.RECEIVEEVENT',
-}
+export const deployAdjudicator = (channelId: any) => ({
+  type: BLOCKCHAIN_DEPLOYADJUDICATOR,
+  channelId,
+});
+export const adjudicatorDeployed = (address: string) => ({
+  type: BLOCKCHAIN_ADJUDICATORDEPLOYED,
+  address,
+});
 
-export const BlockchainAction = {
-  sendTransaction: (transaction: string, wallet: Wallet) => ({
-    type: BlockchainActionType.BLOCKCHAIN_SENDTRANSACTION,
-    transaction,
-    wallet,
-  }),
-  receiveEvent: (event: any) => ({
-    type: BlockchainActionType.BLOCKCHAIN_RECEIVEEVENT,
-    event,
-  }),
-};
+export const metamaskError = (error: any) => ({
+  type: BLOCKCHAIN_METAMASKERROR,
+  error,
+});
+export const wrongNetwork = (networkId: number) => ({
+  type: BLOCKCHAIN_WRONGNETWORK,
+  networkId,
+});
+export const receiveEvent = (event: any) => ({
+  type: BLOCKCHAIN_RECEIVEEVENT,
+  event,
+});
