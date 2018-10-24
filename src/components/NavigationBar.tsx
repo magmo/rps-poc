cabe7cf3567c30c7f688708ef99e96034f92c6d0
Mike Kerzhner <mikerzhner@gmail.com>
2018-10-24 08:26:57 -0700

    Links -> buttons in navbar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-import Navbar from "reactstrap/lib/Navbar";
-import NavItem from "reactstrap/lib/NavItem";
-import NavLink from "reactstrap/lib/NavLink";
+import { Button, Navbar } from "reactstrap";
 import { State } from "fmg-core";
 import { LoginState } from "src/redux/login/reducer";
 
@@ -15,12 +13,12 @@ export default class NavigationBar extends React.PureComponent<Props, State> {
   render() {
     return (
       <Navbar className='navbar'>
-        <NavItem className="navbar-item mr-auto">
-          <NavLink className="navbar-link" href="/rules/">Rules</NavLink>
-        </NavItem>
-        <NavItem className="navbar-item ml-auto">
-          <NavLink className="navbar-link" href="/signout/">Sign out</NavLink>
-        </NavItem>
+        <Button color="link" className="navbar-button mr-auto">
+          Rules
+        </Button>
+        <Button color="link" className="navbar-button ml-auto" onClick={this.props.logoutRequest}>
+          Sign Out
+        </Button>
       </Navbar>
     );
   }
