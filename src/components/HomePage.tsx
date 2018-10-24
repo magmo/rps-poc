5792ca4f3541e899c0afd87289e5f5a92445ffd9
n-white <neil@gladly.com>
2018-10-24 09:17:16 -0700

    Added background image

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -7,19 +7,22 @@ interface IProps {
 
 const HomePage: React.SFC<IProps> = ({ login }) => {
   return (
-    <div className="homePage">
-      <div className="homePage-container">
-        <div className="homePage-title-container">
-          <h1 className={css(styles.title)}>Welcome to Rock, Paper Scissors</h1>
-          <h1 className={css(styles.title)}>State Channel Game</h1>
-        </div>
-        <div className="homePage-loginButton" onClick={login}>
-          <div className="homePage-loginButton-icon-container">
-            <img className="homePage-loginButton-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/32px-Google_%22G%22_Logo.svg.png" />
+    <div>
+      <div className="homePage">
+        <div className="homePage-container">
+          <div className="homePage-title-container">
+            <h1 className={css(styles.title)}>Welcome to Rock, Paper Scissors</h1>
+            <h1 className={css(styles.title)}>State Channel Game</h1>
           </div>
-          <div className="homePage-loginButton-text">Sign in with Google</div>
+          <div className="homePage-loginButton" onClick={login}>
+            <div className="homePage-loginButton-icon-container">
+              <img className="homePage-loginButton-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/32px-Google_%22G%22_Logo.svg.png" />
+            </div>
+            <div className="homePage-loginButton-text">Sign in with Google</div>
+          </div>
         </div>
       </div>
+      <div className="homePage-image"/>
     </div>
   );
 };
