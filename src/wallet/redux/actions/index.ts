49a78e6e6a75fa952c2ec4abca84ff6dd81dd209
Alex Gap <alex.gap@gmail.com>
2018-11-27 12:01:37 -0800

    Initial Challenging logic

--- src/wallet/redux/actions/index.ts
+++ src/wallet/redux/actions/index.ts
@@ -14,7 +14,7 @@ export const keysLoaded = (address: string, privateKey: string) => ({
 export type KeysLoaded = ReturnType<typeof keysLoaded>;
 
 export const CREATE_CHANNEL_REQUEST = ''; // send over opponent addresses, gameLibrary
-                                          // return nonce etc.
+// return nonce etc.
 export const JOIN_CHANNEL_REQUEST = '';
 export const ADDRESS_REQUEST = ''; // provide me with an address
 
@@ -47,24 +47,81 @@ export const DEPOSIT_SUBMITTED = '.'; // when submitted to network
 export const DEPOSIT_CONFIRMED = '.'; // when first seen in a block
 export const DEPOSIT_FINALISED = '.'; // when X blocks deep
 
+export const APPROVE_CHALLENGE = 'WALLET.APPROVE_CHALLENGE';
+export const approveChallenge = () => ({
+  type: APPROVE_CHALLENGE,
+});
+export type ApproveChallenge = ReturnType<typeof approveChallenge>;
+
+export const DECLINE_CHALLENGE = 'WALLET.DECLINE_CHALLENGE';
+export const declineChallenge = () => ({
+  type: DECLINE_CHALLENGE,
+});
+export type DeclineChallenge = ReturnType<typeof declineChallenge>;
+
 export const CHALLENGE_REQUESTED = 'WALLET.CHALLENGE_REQUESTED';
 export const challengeRequested = () => ({
-  type: CHALLENGE_REQUESTED as typeof CHALLENGE_REQUESTED,
+  type: CHALLENGE_REQUESTED,
 });
 export type ChallengeRequested = ReturnType<typeof challengeRequested>;
 
-export const CHALLENGE_INITIATED = '.';
-export const CHALLENGE_SUBMITTED = '.';
-export const CHALLENGE_CONFIRMED = '.';
-export const CHALLENGE_FINALISED = '.';
+export const CHALLENGE_INITIATED = 'WALLET.CHALLENGE_INITIATED';
+export const challengeInitiated = () => ({
+  type: CHALLENGE_INITIATED,
+});
+export type ChallengeInitiated = ReturnType<typeof challengeInitiated>;
+
+export const CHALLENGE_SUBMITTED = 'WALLET.CHALLENGE_SUBMITTED';
+export const challengeSubmitted = () => ({
+  type: CHALLENGE_SUBMITTED,
+});
+export type ChallengeSubmitted = ReturnType<typeof challengeSubmitted>;
+
+
+export const CHALLENGE_CONFIRMED = 'WALLET.CHALLENGE_CONFIRMED';
+export const challengeConfirmed = () => ({
+  type: CHALLENGE_CONFIRMED,
+});
+export type ChallengeConfirmed = ReturnType<typeof challengeConfirmed>;
+
+// TODO: Decide if we want to implement this
+// export const CHALLENGE_FINALIZED = 'WALLET.CHALLENGE_FINALIZED';
 
 export const OPPONENT_CHALLENGE_DETECTED = 'WALLET.OPPONENT_CHALLENGE_DETECTED';
-export const opponentChallengeDetected = () => ({
-  type: OPPONENT_CHALLENGE_DETECTED as typeof OPPONENT_CHALLENGE_DETECTED,
+export const opponentChallengeDetected = (challengeExpiry: Date) => ({
+  type: OPPONENT_CHALLENGE_DETECTED,
+  challengeExpiry,
 });
 export type OpponentChallengeDetected = ReturnType<typeof opponentChallengeDetected>;
 
-export const OPPONENT_CHALLENGE_FINALISED = '.';
+// TODO: Decide if we want to implement this
+// export const OPPONENT_CHALLENGE_FINALISED = '.';
+
+export const CHALLENGE_RESPONSE_RECEIVED = 'WALLET.CHALLENGE_RESPONSE_RECEIVED';
+export const challengeResponseReceived = (position: string) => ({
+  type: CHALLENGE_RESPONSE_RECEIVED,
+  position,
+});
+export type ChallengeResponseReceived = ReturnType<typeof challengeResponseReceived>;
+
+export const CHALLENGE_TIMEOUT = 'WALLET.CHALLENGE_TIMEOUT';
+export const challengeTimeout = () => ({
+  type: CHALLENGE_TIMEOUT,
+});
+export type ChallengeTimeout = ReturnType<typeof challengeTimeout>;
+
+export const ACKNOWLEDGE_CHALLENGE_TIMEOUT = 'WALLET.ACKNOWLEDGE_CHALLENGE_TIMEOUT';
+export const acknowledgeChallengeTimeout = () => ({
+  type: ACKNOWLEDGE_CHALLENGE_TIMEOUT,
+});
+export type AcknowledgeChallengeTimeout = ReturnType<typeof acknowledgeChallengeTimeout>;
+
+export const ACKNOWLEDGE_CHALLENGE_RESPONSE = 'WALLET.ACKNOWLEDGE_CHALLENGE_RESPONSE';
+export const acknowledgeChallengeResponse = () => ({
+  type: ACKNOWLEDGE_CHALLENGE_RESPONSE,
+});
+export type AcknowledgeChallengeResponse = ReturnType<typeof acknowledgeChallengeResponse>;
+
 
 export const CHALLENGE_RESPONSE_INITIATED = '.';
 export const CHALLENGE_RESPONSE_SUBMITTED = '.';
@@ -80,8 +137,11 @@ export const WITHDRAWAL_FINALISED = '.';
 export type WalletAction = (
   | LoggedIn
   | KeysLoaded
-  | OwnPositionReceived
   | OpponentPositionReceived
   | ChallengeRequested
   | OpponentChallengeDetected
+  | ChallengeRequested
+  | ChallengeConfirmed
+  | ChallengeResponseReceived
+  | ChallengeTimeout
 );
--- src/wallet/states/challenging.ts
+++ src/wallet/states/challenging.ts
@@ -4,51 +4,97 @@ import {
 
 export const CHALLENGING = 'CHALLENGING';
 
-export const APPROVE_CHALLENGE = 'APPROVE_CHALLENGE';
+export const APPROVE_CHALLENGE = "APPROVE_CHALLENGE";
 export const INITIATE_CHALLENGE = 'INITIATE_CHALLENGE';
 export const WAIT_FOR_CHALLENGE_CONFIRMATION = 'WAIT_FOR_CHALLENGE_CONFIRMATION';
+
 export const WAIT_FOR_RESPONSE_OR_TIMEOUT = 'WAIT_FOR_RESPONSE_OR_TIMEOUT';
 export const ACKNOWLEDGE_CHALLENGE_RESPONSE = 'ACKNOWLEDGE_CHALLENGE_RESPONSE';
 export const ACKNOWLEDGE_CHALLENGE_TIMEOUT = 'ACKNOWLEDGE_CHALLENGE_TIMEOUT';
 
-interface ApproveChallenge extends AdjudicatorExists {
+export interface ApproveChallenge extends AdjudicatorExists {
   type: typeof APPROVE_CHALLENGE;
   stage: typeof CHALLENGING;
 }
 export function approveChallenge<T extends AdjudicatorExists>(params: T): ApproveChallenge {
-  return { type: APPROVE_CHALLENGE, stage: CHALLENGING, ...adjudicatorExists(params) };
+  return {
+    type: APPROVE_CHALLENGE,
+    stage: CHALLENGING,
+    ...adjudicatorExists(params),
+  };
 }
 
-interface InitiateChallenge extends AdjudicatorExists {
+export interface InitiateChallenge extends AdjudicatorExists {
   type: typeof INITIATE_CHALLENGE;
   stage: typeof CHALLENGING;
 }
+export function initiateChallenge<T extends AdjudicatorExists>(params: T): InitiateChallenge {
+  return {
+    type: INITIATE_CHALLENGE,
+    stage: CHALLENGING,
+    ...adjudicatorExists(params),
+    transactionOutbox: { a: 'b' },
+  };
+}
 
-interface WaitForChallengeConfirmation extends AdjudicatorExists {
+export interface WaitForChallengeConfirmation extends AdjudicatorExists {
   type: typeof WAIT_FOR_CHALLENGE_CONFIRMATION;
   stage: typeof CHALLENGING;
 }
 
-interface WaitForResponseOrTimeout extends AdjudicatorExists {
+export function waitForChallengeConfirmation<T extends AdjudicatorExists>(params: T): WaitForChallengeConfirmation {
+  return {
+    type: WAIT_FOR_CHALLENGE_CONFIRMATION,
+    stage: CHALLENGING,
+    ...adjudicatorExists(params),
+  };
+}
+
+export interface WaitForResponseOrTimeout extends AdjudicatorExists {
   type: typeof WAIT_FOR_RESPONSE_OR_TIMEOUT;
   stage: typeof CHALLENGING;
 }
 
-interface AcknowledgeChallengeResponse extends AdjudicatorExists {
+export function waitForResponseOrTimeout<T extends AdjudicatorExists>(params: T): WaitForResponseOrTimeout {
+  return {
+    type: WAIT_FOR_RESPONSE_OR_TIMEOUT,
+    stage: CHALLENGING,
+    ...adjudicatorExists(params),
+  };
+}
+
+export interface AcknowledgeChallengeResponse extends AdjudicatorExists {
   type: typeof ACKNOWLEDGE_CHALLENGE_RESPONSE;
   stage: typeof CHALLENGING;
 }
 
-interface AcknowledgeChallengeTimeout extends AdjudicatorExists {
+
+export function acknowledgeChallengeResponse<T extends AdjudicatorExists>(params: T): AcknowledgeChallengeResponse {
+  return {
+    type: ACKNOWLEDGE_CHALLENGE_RESPONSE,
+    stage: CHALLENGING,
+    ...adjudicatorExists(params),
+  };
+}
+
+export interface AcknowledgeChallengeTimeout extends AdjudicatorExists {
   type: typeof ACKNOWLEDGE_CHALLENGE_TIMEOUT;
   stage: typeof CHALLENGING;
 }
 
+export function acknowledgeChallengeTimeout<T extends AdjudicatorExists>(params: T): AcknowledgeChallengeTimeout {
+  return {
+    type: ACKNOWLEDGE_CHALLENGE_TIMEOUT,
+    stage: CHALLENGING,
+    ...adjudicatorExists(params),
+  };
+}
+
 export type ChallengingState = (
-  | ApproveChallenge 
-  | InitiateChallenge 
-  | WaitForChallengeConfirmation 
-  | WaitForResponseOrTimeout 
-  | AcknowledgeChallengeResponse 
-  | AcknowledgeChallengeTimeout 
+  | InitiateChallenge
+  | WaitForChallengeConfirmation
+  | WaitForResponseOrTimeout
+  | AcknowledgeChallengeResponse
+  | AcknowledgeChallengeTimeout
+  | ApproveChallenge
 );
