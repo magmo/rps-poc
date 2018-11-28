13e644577223e27bab7ca3ed9e5c90df45b512de
Alex Gap <alex.gap@gmail.com>
2018-11-27 17:33:38 -0800

    Basic response framework

--- src/wallet/states/responding.ts
+++ src/wallet/states/responding.ts
@@ -10,7 +10,8 @@ export const SELECT_CHALLENGE_RESPONSE = 'SELECT_CHALLENGE_RESPONSE';
 export const TAKE_MOVE_IN_APP = 'TAKE_MOVE_IN_APP';
 export const INITIATE_RESPONSE = 'INITIATE_RESPONSE';
 export const WAIT_FOR_RESPONSE_CONFIRMATION = 'WAIT_FOR_RESPONSE_CONFIRMATION';
-
+export const WAIT_FOR_RESPONSE_SUBMISSION = 'WAIT_FOR_RESPONSE_SUBMISSION';
+export const ACKNOWLEDGE_CHALLENGE_COMPLETE = 'ACKNOWLEDGE_CHALLENGE_COMPLETE';
 export interface AcknowledgeChallenge extends AdjudicatorExists {
   type: typeof ACKNOWLEDGE_CHALLENGE;
   stage: typeof RESPONDING;
@@ -43,6 +44,14 @@ export function initiateResponse<T extends AdjudicatorExists>(params: T): Initia
   return { type: INITIATE_RESPONSE, stage: RESPONDING, ...adjudicatorExists(params) };
 }
 
+export interface WaitForResponseSubmission extends AdjudicatorExists {
+  type: typeof WAIT_FOR_RESPONSE_SUBMISSION;
+  stage: typeof RESPONDING;
+}
+export function waitForResponseSubmission<T extends AdjudicatorExists>(params: T): WaitForResponseSubmission {
+  return { type: WAIT_FOR_RESPONSE_SUBMISSION, stage: RESPONDING, ...adjudicatorExists(params) };
+}
+
 export interface WaitForResponseConfirmation extends AdjudicatorExists {
   type: typeof WAIT_FOR_RESPONSE_CONFIRMATION;
   stage: typeof RESPONDING;
@@ -51,10 +60,20 @@ export function waitForResponseConfirmation<T extends AdjudicatorExists>(params:
   return { type: WAIT_FOR_RESPONSE_CONFIRMATION, stage: RESPONDING, ...adjudicatorExists(params) };
 }
 
+export interface AcknowledgeChallengeComplete extends AdjudicatorExists {
+  type: typeof ACKNOWLEDGE_CHALLENGE_COMPLETE;
+  stage: typeof RESPONDING;
+}
+export function acknowledgeChallengeComplete<T extends AdjudicatorExists>(params: T): AcknowledgeChallengeComplete {
+  return { type: ACKNOWLEDGE_CHALLENGE_COMPLETE, stage: RESPONDING, ...adjudicatorExists(params) };
+}
+
 export type RespondingState = (
   | AcknowledgeChallenge
   | ChooseResponse
   | TakeMoveInApp
   | InitiateResponse
+  | WaitForResponseSubmission
   | WaitForResponseConfirmation
+  | AcknowledgeChallengeComplete
 );
