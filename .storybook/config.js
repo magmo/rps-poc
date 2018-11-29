460c97eda11725f1764c0745ac1ec7e6c2b651df
Alex Gap <alex.gap@gmail.com>
2018-11-29 10:00:02 -0800

    Very basic storybook config

---
+++ .storybook/config.js
@@ -0,0 +1,10 @@
+import {
+  configure
+} from '@storybook/react';
+
+function loadStories() {
+  require('../stories/index.jsx');
+  // You can require as many stories as you need.
+}
+
+configure(loadStories, module);
