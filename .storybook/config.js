0a7f34c5aebeefc480cc7485ea78122da4c6ad79
Alex Gap <alex.gap@gmail.com>
2018-11-29 10:09:02 -0800

    Switch to TS

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,8 +3,7 @@ import {
 } from '@storybook/react';
 
 function loadStories() {
-  require('../stories/index.jsx');
+  require('../stories/index.tsx');
   // You can require as many stories as you need.
 }
-
 configure(loadStories, module);
