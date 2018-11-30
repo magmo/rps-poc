fe1acba8ca9faa16ad3713c7c5d432be74663676
Alex Gap <alex.gap@gmail.com>
2018-11-30 15:01:53 -0800

    Test deposit

--- src/wallet/tests/transactions.test.ts
+++ src/wallet/tests/transactions.test.ts
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
 import { Channel } from "fmg-core";
-import { createDeployTransaction } from "../domain/TransactionGenerator";
+import { createDeployTransaction, createDepositTransaction } from "../domain/TransactionGenerator";
 
 import { transactionSender } from "../redux/sagas/transaction-sender";
 import { transactionSentToMetamask, transactionSubmitted, transactionConfirmed, transactionFinalized } from '../redux/actions';
@@ -16,27 +16,24 @@ describe('transactions', () => {
   const participantB = ethers.Wallet.createRandom();
   const participants = [participantA.address, participantB.address];
   const channel = new Channel(gameLibrary, channelNonce, participants);
-
-  beforeEach(async () => {
-
-    const network = await provider.getNetwork();
-    networkId = network.chainId;
-
-  });
-
-  // Currently this test will only work when ganache is running and the contracts are deployed
-  // Need to update the scripts to do this so it passes on the CI server
-  it.skip('should deploy a contract', async () => {
-    // TODO: We need to figure out a better way of testing the saga
+  async function deployContract() {
+    const signer = provider.getSigner();
     const deployTransaction = createDeployTransaction(networkId, channel.id, '0x5');
-    const saga = transactionSender(deployTransaction);
+    const transactionReceipt = await signer.sendTransaction(deployTransaction);
+    const confirmedTransaction = await transactionReceipt.wait();
+    return confirmedTransaction.contractAddress;
+  }
+
+  async function testTransactionSender(transactionToSend) {
+    // TODO: We need to figure out a better way of testing the saga
+    const saga = transactionSender(transactionToSend);
     saga.next();
     expect(saga.next(provider).value).toEqual(put(transactionSentToMetamask()));
     const signer = provider.getSigner();
-    const transaction = await signer.sendTransaction(deployTransaction);
+    const transactionReceipt = await signer.sendTransaction(transactionToSend);
     saga.next();
-    expect(saga.next(transaction).value).toEqual(put(transactionSubmitted()));
-    const confirmedTransaction = await transaction.wait();
+    expect(saga.next(transactionReceipt).value).toEqual(put(transactionSubmitted()));
+    const confirmedTransaction = await transactionReceipt.wait();
     saga.next();
     expect(saga.next(confirmedTransaction).value).toEqual(put(transactionConfirmed(confirmedTransaction.contractAddress)));
     // We don't actually bother waiting for 5 confirmations as that will make the test take too long
@@ -44,5 +41,23 @@ describe('transactions', () => {
     expect(saga.next(confirmedTransaction).value).toEqual(put(transactionFinalized()));
     expect(saga.next().done).toBe(true);
 
+  }
+
+  beforeEach(async () => {
+
+    const network = await provider.getNetwork();
+    networkId = network.chainId;
+
+  });
+  // Currently this test will only work when ganache is running and the contracts are deployed
+  // Need to update the scripts to do this so it passes on the CI server => {
+  it.skip('should deploy the contract', async () => {
+    const deployTransaction = createDeployTransaction(networkId, channel.id, '0x5');
+    await testTransactionSender(deployTransaction);
+  });
+  it.skip('should deposit into the contract', async () => {
+    const contractAddress = await deployContract() as string;
+    const depositTransaction = createDepositTransaction(contractAddress, '0x5');
+    await testTransactionSender(depositTransaction);
   });
 });
