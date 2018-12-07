5181a73e8bd1f35350b759e9c45ee20ea2ba7423
Mike Kerzhner <mikerzhner@gmail.com>
2018-12-07 10:39:58 -0800

    Supressing react error

--- src/wallet/components/SidebarLayout.tsx
+++ src/wallet/components/SidebarLayout.tsx
@@ -8,12 +8,12 @@ import Modal from 'react-modal';
 
 const customStyles = {
   content: {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
     width: '500px',
   },
   overlay: {
@@ -24,7 +24,7 @@ const customStyles = {
 export default class SidebarLayout extends PureComponent {
   render() {
     return (
-      <Modal isOpen={true} style={customStyles}>
+      <Modal isOpen={true} style={customStyles} ariaHideApp={false}>
         {this.props.children}
       </Modal>
     );
